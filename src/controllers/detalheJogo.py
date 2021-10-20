a70dc887f77f035ea8c3934767dcc993b7858b0e
Matheus Oliveira <teteuoliveira12@gmail.com>
2021-10-19 23:32:59 -0300

    Final

--- src/controllers/detalheJogo.py
+++ src/controllers/detalheJogo.py
@@ -2,7 +2,8 @@ from flask import render_template,request
 import requests
 
 def jogoInfo():
-    id = request.args.get('id')
+    id = request.args.get('id', default=1)
+    
     url = f"https://www.freetogame.com/api/game?id={id}"
     req = requests.get(url).json()
     return render_template( 'infoJogo.html', jogo=req )
--- src/controllers/jogos.py
+++ src/controllers/jogos.py
@@ -5,8 +5,6 @@ from pprint import pprint
 def mostrarJogos():
     plataform = request.args.get('platform')
 
-    print(plataform)
-
     url = "https://www.freetogame.com/api/games?platform=all" if plataform == None  else f"https://www.freetogame.com/api/games?platform={plataform}"
     req = requests.get(url).json()
     return render_template("jogos.html",jogosPlataform=req)
