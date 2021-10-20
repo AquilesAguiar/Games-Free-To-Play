aa39c017e1444784f5cebe4f2ddc3f903c5deacc
Aquiles Aguiar <aquilesvibe@gmail.com>
2021-10-19 23:39:37 -0300

    remoção de arquivos

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
-# from flask_ngrok import run_with_ngrok
 from src.routes.index import index
 from src.routes.jogos import jogos
 from src.routes.detalheJogo import detalheJogo
 
 app = Flask(__name__,template_folder='src\\templates',static_folder='src\\static')
-# run_with_ngrok(app)
 
 app.register_blueprint(index)
 app.register_blueprint(jogos,url_prefix='/jogos')
